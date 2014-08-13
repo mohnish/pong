73a4d3b517f01f5564dc2911f64645ffcfc42108
Mohnish Thallavajhula <i@mohni.sh>
2014-08-12 18:39:35 -0700

    add ball

--- ball.js
+++ ball.js
@@ -5,3 +5,11 @@ function Ball() {
 
 Ball.prototype = Object.create(Entity.prototype);
 Ball.prototype.constructor = Ball;
+
+Ball.prototype.draw = function() {
+  Entity.prototype.draw.apply(this, arguments);
+};
+
+Ball.prototype.update = function() {
+  Entity.prototype.update.apply(this, arguments);
+};
