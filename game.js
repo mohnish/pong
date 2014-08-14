e26664177822283c14cada35de7dab42a1a8f2c8
Mohnish Thallavajhula <i@mohni.sh>
2014-08-13 19:00:45 -0700

    wip

--- game.js
+++ game.js
@@ -5,12 +5,22 @@ function Game(canvas) {
 }
 
 Game.prototype.start = function() {
-  var self = this;
+  var self = this
+    , fps = 60 // ms per frame
+    , interval = 1000/fps;
 
   setInterval(function() {
     self.update();
     self.draw();
-  }, 1000/60);
+  }, interval);
+};
+
+Game.keys = {
+  32: 'space',
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down'
 };
 
 Game.prototype.update = function() {
--- pong.js
+++ pong.js
@@ -1,10 +1,15 @@
 
-var canvas = document.querySelector('canvas');
-var game = new Game(canvas);
+function Background() {}
+
+Background.prototype.draw = function(context) {
+  context.fillStyle = '#000';
+  context.fillRect(0, 0, game.width, game.height);
+};
+
+var game = new Game($('canvas')[0]);
 
 game.entities = [
-  new Background(),
-  new Ball()
+  new Background()
 ];
 
 game.start();
