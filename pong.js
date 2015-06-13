0f547a00dd29d70c26fe88004a7184419ef953c7
Mohnish Thallavajhula <i@mohni.sh>
2015-06-13 12:20:18 -0700

    extract bg out to its own file

--- pong.js
+++ pong.js
@@ -1,13 +1,6 @@
 
-function Background() {}
-
-Background.prototype.draw = function(context) {
-  context.fillStyle = '#000';
-  context.fillRect(0, 0, game.width, game.height);
-};
-
-var canvas = document.querySelector('canvas')
-  , game = new Game(canvas);
+var canvas = document.querySelector('canvas');
+var game = new Game(canvas);
 
 game.entities = [
   new Background(),
